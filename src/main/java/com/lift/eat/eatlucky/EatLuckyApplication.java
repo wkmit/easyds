34981b36a804e463bb16b93cf83cc52956a4a28d
wangyunpeng <18211653872@163.com>
2020-12-17 20:09:29 +0800

    first commit

---
+++ src/main/java/com/lift/eat/eatlucky/EatLuckyApplication.java
@@ -0,0 +1,13 @@
+package com.lift.eat.eatlucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatLuckyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatLuckyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lift/eat/eatlucky/EatLuckyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lift.eat.eatlucky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatLuckyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
